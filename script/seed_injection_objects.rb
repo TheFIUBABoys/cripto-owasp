b2708f8b875a30afa6045d1c3b4e20d028cdf38c
Gonzalo Beviglia <gonzalo.beviglia@restorando.com>
2015-06-29 14:17:17 -0300

    Seed.

--- script/seed_injection_objects.rb
+++ script/seed_injection_objects.rb
@@ -4,6 +4,6 @@
 
 count = SqlInjectionObject.count
 
-(count...20).each do |_n|
+(count...5).each do |_n|
   SqlInjectionObject.create title: Forgery('address').country, text: Forgery('address').street_address
 end
